32e91f2742fa0103dc3cdd110eefffb37b3b3872
micheal-olatunbosun <molatunbosun7@gmail.com>
2025-10-26 20:19:20 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PAFT-Finance/", // 👈 Important for GitHub Pages
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/PAFT-Finance/", // 👈 VERY IMPORTANT for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "::",
+    port: 8080,
   },
 });
